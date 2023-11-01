4cd6ba738a8cca8a712a16f29c95bd89473c0402
Justnoones <yanw97841@gmail.com>
2023-11-01 10:54:16 +0630

    alternative-of-getting-params

--- src/app/api/todos/[id]/route.js
+++ src/app/api/todos/[id]/route.js
@@ -2,8 +2,8 @@ import { NextResponse } from "next/server";
 
 const DATA_SOURCE_URL = "https://jsonplaceholder.typicode.com/todos";
 
-export const GET = async (request) => {
-    const id = request.url.slice(request.url.lastIndexOf("/") + 1);
+export const GET = async (request, {params : { id }}) => {
+    // const id = request.url.slice(request.url.lastIndexOf("/") + 1);
 
     const res = await fetch(`${DATA_SOURCE_URL}/${id}`);
     const todo = await res.json();
